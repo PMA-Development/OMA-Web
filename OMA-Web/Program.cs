fed8e194af18a2a1ab26181937fea5ea0f02d2dc
rene069 <rene_rasmussen@live.dk>
2024-11-05 15:03:06 +0100

    Started work on adding Authentication to the Webapp still WIP

--- OMA-Web/Program.cs
+++ OMA-Web/Program.cs
@@ -12,9 +12,29 @@ namespace OMA_Web
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
+
+            var oidcSettings = builder.Configuration.GetSection("OidcSettings");
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                options.ProviderOptions.Authority = oidcSettings["Authority"];
+                options.ProviderOptions.ClientId = oidcSettings["ClientId"];
+
+                options.ProviderOptions.ResponseType = oidcSettings["ResponseType"];
+                //options.ProviderOptions.RedirectUri = oidcSettings["RedirectUri"];
+                options.ProviderOptions.PostLogoutRedirectUri = oidcSettings["PostLogoutRedirectUri"];
+
+                // Adding scopes
+                foreach (var scope in oidcSettings.GetSection("DefaultScopes").Get<string[]>())
+                {
+                    options.ProviderOptions.DefaultScopes.Add(scope);
+                }
+            });
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddBlazorBootstrap();
             builder.Services.AddRadzenComponents();
+
+
 
             await builder.Build().RunAsync();
         }
